479fee13dcd3971c207ac33f58187c64a70e6121
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-07-26 16:56:34 +1200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ async def dm(ctx, member : discord.Member = None, *, message):
         return await client.say(ctx.message.author.mention + "Specify a user to DM!")
     if member == "@everyone":
         for server_member in ctx.message.server.members:
-        await client.send_message(server_member, message)
+            await client.send_message(server_member, message)
     else:
         await client.send_message(member, message)
 
